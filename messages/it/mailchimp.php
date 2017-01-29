1e015ba853f1744618bc613380aafb77d5f94802
Giandomenico Olini <giando.olini@gogodigital.it>
2017-01-29 21:24:54 +0100

    Adding Translations

---
+++ messages/it/mailchimp.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-mailchimp
+ * @license BSD 3-Clause
+ * @package yii2-mailchimp
+ * @version 0.0.1
+ */
+
+return array(
+    'First Name' => 'Nome',
+    'Last Name' => 'Cognome',
+    'Name' => 'Nome',
+    'Submit' => 'Invia',
+    'Subscribe' => 'Iscriviti',
+);
