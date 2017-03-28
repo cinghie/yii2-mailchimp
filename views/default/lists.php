b08b4f1a484f34ba55292b94f8ccd5df77166c14
Giandomenico Olini <giando.olini@gogodigital.it>
2017-03-28 22:12:32 +0200

    Fix No Lists error

--- views/default/lists.php
+++ views/default/lists.php
@@ -7,26 +7,32 @@ $this->title = Yii::t('mailchimp', 'Lists');
 $this->params['breadcrumbs'][] = Yii::t('mailchimp', 'Newsletters');
 $this->params['breadcrumbs'][] = $this->title;
 
-$html = '<table class="table table-bordered table-responsive">';
-$html .= '<thead>';
-$html .= '<tr>
+if($lists)
+{
+    $html = '<table class="table table-bordered table-responsive">';
+    $html .= '<thead>';
+    $html .= '<tr>
             <th>'.Yii::t('mailchimp','List Name').'</th>
             <th class="text-center">'.Yii::t('mailchimp','Member Count').'</th>
             <th class="text-center">'.Yii::t('mailchimp','Unsubscribe Count').'</th>
             <th class="text-center">'.Yii::t('mailchimp','ID').'</th>
           </tr>';
-$html .= '<tbody>';
+    $html .= '<tbody>';
 
+    foreach($lists as $list) {
+        $html .= '<tr>';
+        $html .= '<td><a href="'.Url::to([ 'default/list', 'id' => $list['id'], 'name' => $list['name'] ]).'">'.$list['name'].'</a></td>';
+        $html .= '<td class="text-center" width="8%">'.$list['stats']['member_count'].'</td>';
+        $html .= '<td class="text-center" width="8%">'.$list['stats']['unsubscribe_count'].'</td>';
+        $html .= '<td class="text-center" width="8%">'.$list['id'].'</td>';
+        $html .= '</tr>';
+    }
 
-foreach($lists as $list) {
-    $html .= '<tr>';
-    $html .= '<td><a href="'.Url::to([ 'default/list', 'id' => $list['id'], 'name' => $list['name'] ]).'">'.$list['name'].'</a></td>';
-    $html .= '<td class="text-center" width="8%">'.$list['stats']['member_count'].'</td>';
-    $html .= '<td class="text-center" width="8%">'.$list['stats']['unsubscribe_count'].'</td>';
-    $html .= '<td class="text-center" width="8%">'.$list['id'].'</td>';
-    $html .= '</tr>';
+    $html .= '</tbody></table>';
+
+} else {
+
+    $html = '<div class="alert alert-danger">'.Yii::t('mailchimp', 'No Lists Found').'</div>';
 }
-
-$html .= '</tbody></table>';
 
 echo $html;
